62065bb29c60968ddc8d5c39588b4c3ffac6b436
PhoebePudge <55756132+PhoebePudge@users.noreply.github.com>
2021-11-22 15:15:50 +0000

    .
    Added Case for metal materials not being found
    show an error if trying to use more metal than you have
    reformed metal height calculation to fit in a height of 1 block

--- Assets/SmelteryController.cs
+++ Assets/SmelteryController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 public class SmelteryController : MonoBehaviour {
+
     public static Dictionary<Metals, int> oresStored = new Dictionary<Metals, int>();
     public TextMeshProUGUI output;
-    public static void AddItem(string itemType, int quantity) {
-        
+
+    public static void AddItem(string itemType, int quantity) { 
         bool worked;
         Metals metal = StringToMetal(itemType, out worked);
 
@@ -24,15 +25,17 @@ public class SmelteryController : MonoBehaviour {
     public static void RemItem(string itemType, int quantity) { 
         bool worked;
         Metals metal = StringToMetal(itemType, out worked);
+
         Debug.Log("Rem " + itemType + quantity + " : Status -> " + worked);
+
         if (worked) { 
             if (oresStored.ContainsKey(metal)) {
                 oresStored[metal] -= quantity;
-                if (oresStored[metal] <= 0) {
-                    //show and error if trying to use more metal than you have 
+                if (oresStored[metal] <= 0) { 
+                    Debug.LogError("you tried to use more materials than you have");
                 }
             } else {
-                //no metal remaining
+                Debug.LogError("you tried to use more materials than you have");
             }
         }
         
@@ -41,48 +44,51 @@ public class SmelteryController : MonoBehaviour {
     private static GameObject[] visualMetals = new GameObject[4];
     public Material[] metalMaterials = new Material[4];
 
+    int lastKnownTotalHeight = 1;
     private void Update() {
         output.text = "";
         int index = 0;
-        int height = 0;
-        float multipler = .1f;
+        int height = -1;
+        float multipler = 1f / (float)(lastKnownTotalHeight); 
         foreach (var item in oresStored) {
-
-            if (visualMetals[index] == null) {
-                visualMetals[index] = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                visualMetals[index].name = item.Key.ToString();
-                visualMetals[index].transform.SetParent(transform);
-
-                //height and scale is somehow messed up
-                visualMetals[index].transform.localPosition = new Vector3(0,(height + (item.Value / 2)) * multipler, 0); 
-                visualMetals[index].transform.localScale = new Vector3(1, item.Value * multipler, 1);
-
-                //change to less intensive method (dictionary?)
-                foreach (var material in metalMaterials) {
-                    if (material.name == item.Key.ToString()) {
-                        visualMetals[index].GetComponent<MeshRenderer>().material = material;
-                        break;
-                    }
-                } 
-
-                //add in case for material not found
-
-
-            } else { 
-                visualMetals[index].transform.localPosition = new Vector3(0, height * multipler, 0);
-                visualMetals[index].transform.localScale = new Vector3(1, item.Value * multipler, 1);
-            }
-
             if (item.Value == 0) {
                 visualMetals[index].SetActive(false);
-                oresStored.Remove(item.Key); 
-            } else {
-                visualMetals[index].SetActive(true);
-                index++;
+                oresStored.Remove(item.Key);
+            } else { 
                 height += item.Value;
                 output.text += item.Key.ToString() + " : " + item.Value.ToString() + " \n";
+
+                if (visualMetals[index] == null) {
+                    visualMetals[index] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    visualMetals[index].name = item.Key.ToString();
+                    visualMetals[index].transform.SetParent(transform);
+
+                    //height and scale is somehow messed up
+                    visualMetals[index].transform.localPosition = new Vector3(0, (height - (item.Value / 2)), 0);
+                    visualMetals[index].transform.localScale = new Vector3(1, item.Value, 1);
+
+                    //change to less intensive method (dictionary?)
+                    bool materialFound = false;
+                    foreach (var material in metalMaterials) {
+                        if (material.name == item.Key.ToString()) {
+                            visualMetals[index].GetComponent<MeshRenderer>().material = material;
+                            materialFound = true;
+                            break;
+                        }
+                    }
+                     
+                    if (materialFound) {
+                        Debug.LogWarning("No material has been set for this metal, default used");
+                    }
+                } else {
+                    visualMetals[index].transform.localPosition = new Vector3(0, height * multipler, 0);
+                    visualMetals[index].transform.localScale = new Vector3(1, item.Value * multipler, 1);
+                } 
+                visualMetals[index].SetActive(true);
             }
+            index++;
         }
+        lastKnownTotalHeight = height;
     }
 
 
@@ -94,7 +100,7 @@ public class SmelteryController : MonoBehaviour {
         if (!result) {
             Debug.LogError("Parse Error, Unknown Enum!");
         }
-        //handle no enum being found
+
         return metal;
     }
 }
