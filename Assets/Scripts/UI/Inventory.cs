2c9cd8447a9ae93ea0ec08d41771e061481cfc1d
CLaDOS-SODaLC <55404148+LewisConor@users.noreply.github.com>
2021-12-01 12:14:55 +0000

    Push to Pull

--- Assets/Scripts/UI/Inventory.cs
+++ Assets/Scripts/UI/Inventory.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Inventory : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class Inventory : MonoBehaviour
 {
     UIHandler uiH;
     GameManager gM;
@@ -55,17 +55,8 @@ public class Inventory : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
         }
     }
 
-    //Detect if the Cursor starts to pass over the GameObject
-    public void OnPointerEnter(PointerEventData pointerEventData)
+    public void ContextMenu()
     {
-        //Output to console the GameObject's name and the following message
-        Debug.Log("Cursor Entering " + name + " GameObject");
-    }
 
-    //Detect when Cursor leaves the GameObject
-    public void OnPointerExit(PointerEventData pointerEventData)
-    {
-        //Output the following message with the GameObject's name
-        Debug.Log("Cursor Exiting " + name + " GameObject");
     }
 }
