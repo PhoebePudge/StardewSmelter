158c3396aae0d08a1ab617ba1a2bab7b29deb250
PhoebePudge <55756132+PhoebePudge@users.noreply.github.com>
2022-01-08 11:00:27 +0000

    .

--- Assets/Scripts/Movement/OutsideWorldCamera.cs
+++ Assets/Scripts/Movement/OutsideWorldCamera.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class OutsideWorldCamera : MonoBehaviour
 {
+    private float zoomOffset;
+    [SerializeField] float cameraDistance = 10f;
     [SerializeField] Transform rotationPoint;
     [SerializeField] Transform playerPoint;
     // Start is called before the first frame update
@@ -15,7 +17,22 @@ public class OutsideWorldCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(rotationPoint);
-        gameObject.transform.position = playerPoint.transform.position + (transform.forward * -10);
+        Quaternion rotation = Quaternion.Lerp(
+            Quaternion.LookRotation(rotationPoint.position - transform.position),
+            transform.rotation,
+            Time.deltaTime);
+
+        transform.rotation = rotation;
+
+        zoomOffset += Input.mouseScrollDelta.y;
+        zoomOffset = Mathf.Clamp(zoomOffset, -10f, 10f);
+        Vector3 scrollOffset = transform.forward * zoomOffset;
+
+
+        gameObject.transform.position = 
+            Vector3.Lerp( 
+                playerPoint.transform.position + (transform.forward * -cameraDistance) + scrollOffset,
+                gameObject.transform.position,
+                Time.deltaTime);
     }
 }
